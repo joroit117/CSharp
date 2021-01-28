292781b39d985d15e412855221c75b93b20a099e
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-28 22:42:32 +0200

    Add files via upload

---
+++ First-Steps-In-Coding-Exercise/ConerterDollarsToBGN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConerterDollarsToBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double courseOfDollars = 1.79549;
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * courseOfDollars;
+            Console.WriteLine($"{bgn:f2}");
+        }
+    }
+}

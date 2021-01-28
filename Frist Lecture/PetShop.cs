7e82ff9683134110ad2b486f8f5ad279548e6ec6
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-28 22:22:01 +0200

    Add files via upload

---
+++ Frist Lecture/PetShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const double price1 = 2.50;
+            //const double price2 = 4;
+            int dogs = int.Parse(Console.ReadLine());
+            int neighbourDogs = int.Parse(Console.ReadLine());
+            double sum = dogs * 2.5 + neighbourDogs * 4;
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
---
+++ Frist Lecture/YardGreening.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pricePerSM = 7.61;
+            const double discount = 0.18;
+            double squareMeters = double.Parse(Console.ReadLine());
+            double dis = pricePerSM * squareMeters * discount;
+            double finalPrice = pricePerSM * squareMeters - dis;
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {dis} lv.");
+        }
+    }
+}

ce9e0bf0ddbc6a8fa6a71240a9f3179d417e2321
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-28 22:28:23 +0200

    Add files via upload

---
+++ SecondLecture/FruitMarket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FruitMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceStrawberries = double.Parse(Console.ReadLine());
+            double bananas = double.Parse(Console.ReadLine());
+            double oranges = double.Parse(Console.ReadLine());
+            double raspberries = double.Parse(Console.ReadLine());
+            double strawberies = double.Parse(Console.ReadLine());
+            double priceRaspberries = priceStrawberries / 2;
+            double priceOranges = priceRaspberries - 0.4 * priceRaspberries;
+            double priceBananas = priceRaspberries - 0.8 * priceRaspberries;
+            double sum = strawberies * priceStrawberries + raspberries * priceRaspberries + bananas * priceBananas + oranges * priceOranges;
+            Console.WriteLine($"{sum:f2}");
+
+        }
+    }
+}

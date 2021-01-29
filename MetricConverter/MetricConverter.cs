f967c2e56ac145aa8097181e183411ea3731c55f
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-29 12:24:25 +0200

    Add files via upload

---
+++ MetricConverter/MetricConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+            if(input == "m" && output == "cm")
+            {
+                number *= 100;
+            }
+            else if (input == "m" && output == "mm")
+            {
+                number *= 1000;
+            }
+            else if (input == "cm" && output == "m")
+            {
+                number *= 0.01;
+            }
+            else if (input == "cm" && output == "mm")
+            {
+                number *= 10;
+            }
+            else if (input == "mm" && output == "m")
+            {
+                number *= 0.001;
+            }
+            else if (input == "mm" && output == "cm")
+            {
+                number *= 0.1;
+            }
+            Console.WriteLine($"{number:f3}");
+        }
+    }
+}
---
+++ Scolarship/Scolarship.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Scolarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double average = double.Parse(Console.ReadLine());
+            double minSalary = double.Parse(Console.ReadLine());
+
+            double scholarshipForExcellentResults = Math.Floor(average * 25);
+            double socialScholarship = Math.Floor(0.35 * minSalary);
+
+            if (average >= 5.5 && income >= minSalary)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results {scholarshipForExcellentResults} BGN");
+            }
+            else if (income >= minSalary && average < 5.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+            else if(average >= 5.5 && income < minSalary)
+            {
+                if (scholarshipForExcellentResults > socialScholarship) 
+                    Console.WriteLine($"You get a scholarship for excellent results {scholarshipForExcellentResults} BGN");
+                else
+                    Console.WriteLine($"You get a Social scholarship {socialScholarship} BGN");
+            }
+            else if(average >= 4.5 && income < minSalary)
+            {
+                Console.WriteLine($"You get a Social scholarship {socialScholarship} BGN");
+            }
+            else if(income < minSalary && average < 4.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+        }
+    }
+}

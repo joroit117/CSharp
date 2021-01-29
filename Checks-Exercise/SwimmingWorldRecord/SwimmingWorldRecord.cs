f3c1e1996a30f0e96145f5181337b1a89cf781be
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-29 12:21:46 +0200

    Add files via upload

---
+++ Checks-Exercise/SwimmingWorldRecord/SwimmingWorldRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwimmingWorldRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+            double totalTime = meters * timePerMeter;
+            totalTime += Math.Floor(meters / 15) * 12.5;
+            if(totalTime < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalTime - record:f2} seconds slower.");
+            }
+        }
+    }
+}
---
+++ Checks-Exercise/Time+15Minutes/Time+15Minutes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Time_15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            if(minutes >= 45 && hours != 23)
+            {
+                hours++;
+                minutes = minutes - 45;
+            }
+            else if (minutes >= 45 && hours == 23)
+            {
+                hours = 0;
+                minutes = minutes - 45;
+            }
+            else
+            {
+                minutes += 15;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}

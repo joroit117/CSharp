40cb1a394fe22ff50cb63fb3df16b319b145b2f8
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-30 12:44:46 +0200

    Add files via upload

---
+++ FirstStepsInCoding-Exercise/CharityCampaign/CharityCampaign.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double cake = 45;
+            const double waffle = 5.8;
+            const double pancake = 3.2;
+            int days = int.Parse(Console.ReadLine());
+            int bakers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+            double sumPerDay = (cake * cakes + waffle * waffles + pancake * pancakes) * bakers;
+            double wholeSum = sumPerDay * days;
+            double wholeSumTax = wholeSum - wholeSum / 8;
+            Console.WriteLine(wholeSumTax);
+        }
+    }
+}

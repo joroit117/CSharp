210f590c40b8e10ef5182542a845ad78a39f1aad
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-30 12:41:01 +0200

    Add files via upload

---
+++ FirstStepsInCoding-Exercise/VacationBookList/VacationBookList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VacationBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPages = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int numberOfDays = int.Parse(Console.ReadLine());
+            double timeForBook = numberOfPages / pagesPerHour;
+            double amountOfTimeNeed = timeForBook / numberOfDays;
+            Console.WriteLine(amountOfTimeNeed);
+        }
+    }
+}

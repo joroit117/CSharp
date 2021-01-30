5bdfa0dbb56ff4958c6bdbe5b4148c29fc958864
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-30 12:56:18 +0200

    Add files via upload

---
+++ Checks-Exercise/GodzillaVsKong/GodzillaVsKong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int scripters = int.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+            double decorSum = budget * 0.10;
+            double scriptersSum = scripters * price;
+            if(scripters > 150)
+            {
+                scriptersSum -= scriptersSum * 0.10;
+            }
+            double total = budget - scriptersSum - decorSum;
+            if (budget >= decorSum + scriptersSum)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {total:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {-total:f2} leva more.");
+            }
+        }
+    }
+}

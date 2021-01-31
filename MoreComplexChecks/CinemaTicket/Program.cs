724719ebf35c6dbc3f85e450f5af2a8f1b226004
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-31 18:30:27 +0200

    Add files via upload

---
+++ MoreComplexChecks/CinemaTicket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CinemaTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+            int price = 0;
+            switch(dayOfWeek)
+            {
+                case "Monday":
+                    price = 12;
+                    break;
+                case "Tuesday":
+                    price = 12;
+                    break;
+                case "Wednesday":
+                    price = 14;
+                    break;
+                case "Thursday":
+                    price = 14;
+                    break;
+                case "Friday":
+                    price = 12;
+                    break;
+                case "Saturday":
+                    price = 16;
+                    break;
+                case "Sunday":
+                    price = 16;
+                    break;
+            }
+            Console.WriteLine(price);
+        }
+    }
+}

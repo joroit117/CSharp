8cd9231ec1a47c26155eeab35e3d55f230455b64
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-01-31 18:23:35 +0200

    Add files via upload

---
+++ MoreComplexChecks/TradeComissions/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace TradeComissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double comission = 0;
+            bool isValid = true;
+            if(city == "Sofia")
+            {
+                if(sales >= 0 && sales <= 500)
+                {
+                    comission = 0.05;
+                }
+                else if (sales >= 500 && sales <= 1000)
+                {
+                    comission = 0.07;
+                }
+                else if (sales >= 1000 && sales <= 10000)
+                {
+                    comission = 0.08;
+                }
+                else if (sales >= 10000)
+                {
+                    comission = 0.12;
+                }
+                else
+                {
+                    isValid = false;
+                } 
+            }
+            else if (city == "Varna")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = 0.045;
+                }
+                else if (sales >= 500 && sales <= 1000)
+                {
+                    comission = 0.075;
+                }
+                else if (sales >= 1000 && sales <= 10000)
+                {
+                    comission = 0.1;
+                }
+                else if (sales >= 10000)
+                {
+                    comission = 0.13;
+                }
+                else
+                {
+                    isValid = false;
+                }
+            }
+            else if (city == "Plovdiv")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = 0.055;
+                }
+                else if (sales >= 500 && sales <= 1000)
+                {
+                    comission = 0.08;
+                }
+                else if (sales >= 1000 && sales <= 10000)
+                {
+                    comission = 0.12;
+                }
+                else if (sales >= 10000)
+                {
+                    comission = 0.145;
+                }
+                else
+                {
+                    isValid = false;
+                }
+            }
+            else
+            {
+                isValid = false;
+            }
+            if(isValid)
+            {
+                Console.WriteLine($"{sales * comission:f2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
---
+++ MoreComplexChecks/WorkingHours/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+            if(dayOfWeek != "Sunday" && time >= 10 && time <= 18)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}

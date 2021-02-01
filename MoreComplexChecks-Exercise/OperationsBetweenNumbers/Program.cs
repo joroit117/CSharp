2a343994e3f4ab7b4568abb8c64470d86ad6aef4
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-01 23:03:09 +0200

    Add files via upload

---
+++ MoreComplexChecks-Exercise/OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+            string operator1 = Console.ReadLine();
+
+            switch(operator1)
+            {
+                case "+":
+                    if((number1 + number2) % 2 == 0)
+                    {
+                        Console.WriteLine($"{number1} + {number2} = {number1 + number2} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} + {number2} = {number1 + number2} - odd");
+                    }
+                    break;
+                case "-":
+                    if ((number1 - number2) % 2 == 0)
+                    {
+                        Console.WriteLine($"{number1} - {number2} = {number1 - number2} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} - {number2} = {number1 - number2} - odd");
+                    }
+                    break;
+                case "*":
+                    if ((number1 * number2) % 2 == 0)
+                    {
+                        Console.WriteLine($"{number1} * {number2} = {number1 * number2} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} * {number2} = {number1 * number2} - odd");
+                    }
+                    break;
+                case "/":
+                    if(number2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {number1} by zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} / {number2} = {number1 / number2:f2}");
+                    }
+                    break;
+                case "%":
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {number1} by zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} % {number2} = {number1 % number2}");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ MoreComplexChecks-Exercise/Volleyball/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            double holidays = int.Parse(Console.ReadLine());
+            double weekends = int.Parse(Console.ReadLine());
+            double games = 48;
+
+            games -= weekends;
+            games *= 0.75;
+            games += weekends;
+            games += (holidays * 2 / 3);
+
+            if (year == "leap")
+            {
+                games += 0.15 * games;
+            }
+            Console.WriteLine(Math.Floor(games));
+
+        }
+    }
+}

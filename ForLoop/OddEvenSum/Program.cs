266b399d2bea7c8a7a760647c51dd089e04ab29b
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-02 16:32:45 +0200

    Add files via upload

---
+++ ForLoop/OddEvenSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumEven = 0;
+            int sumOdd = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if(i % 2 == 0)
+                {
+                    sumEven += number;
+                }
+                else
+                {
+                    sumOdd += number;
+                }
+            }
+            if(sumEven == sumOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + sumEven);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(sumEven - sumOdd));
+            }
+        }
+    }
+}

8a2e407e1d15f66a7ee59bfde9847a9a7b1d38a7
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-02 16:58:11 +0200

    Add files via upload

---
+++ ForLoop/CleverLily/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double priceWashingMachine = double.Parse(Console.ReadLine());
+            double priceToys = double.Parse(Console.ReadLine());
+
+            int toysCount = 0;
+            double savedMoney = 0;
+            double moneyGift = 10;
+
+            for(int i = 1; i <= age; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    savedMoney += moneyGift;
+                    savedMoney--;
+
+                    moneyGift += 10;
+                }
+                else
+                {
+                    toysCount++;
+                }
+            }
+            double moneyFromToys = toysCount * priceToys;
+            savedMoney += moneyFromToys;
+
+            if(savedMoney >= priceWashingMachine)
+            {
+                Console.WriteLine($"Yes! {savedMoney - priceWashingMachine:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {priceWashingMachine - savedMoney:f2}");
+            }
+        }
+    }
+}
---
+++ ForLoop/LeftAndRightSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 0; i < n*2; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if(i < n)
+                {
+                    sum1 += number;
+                }
+                else
+                {
+                    sum2 += number;
+                }
+            }
+            if(sum1 == sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum1}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sum1 - sum2)}");
+            }
+        }
+    }
+}

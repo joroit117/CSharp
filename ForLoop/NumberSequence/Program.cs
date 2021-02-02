5ec048f823ee0113827d4f10502f315ddeca740b
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-02 16:44:12 +0200

    Add files via upload

---
+++ ForLoop/NumberSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = Int32.MaxValue;
+            int max = Int32.MinValue;
+            for(int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if(number > max)
+                {
+                    max = number;
+                }
+                if(number < min)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}

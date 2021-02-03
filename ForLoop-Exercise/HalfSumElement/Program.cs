fc04d4f5a881e1e67218f505268b302c681808d5
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-03 23:23:27 +0200

    Add files via upload

---
+++ ForLoop-Exercise/HalfSumElement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = Int32.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                if(number > max)
+                {
+                    max = number;
+                }
+            }
+            if(max == sum - max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(max - (sum - max)));
+            }
+        }
+    }
+}

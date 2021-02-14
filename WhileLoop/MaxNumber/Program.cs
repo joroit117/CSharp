bedb3d0398096aaf3a7503455d08454bb45cc119
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-14 21:27:10 +0200

    Add files via upload

---
+++ WhileLoop/MaxNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int max = Int32.MinValue;
+            while(number != "Stop")
+            {
+                int num = int.Parse(number);
+                if(num > max)
+                {
+                    max = num;
+                }
+                number = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ WhileLoop/Moving/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int storage = width * length * height;
+            int boxesCount = 0;
+            bool endOfBoxes = false;
+            
+            while(boxesCount <= storage)
+            {
+                string boxes = Console.ReadLine();
+                if(boxes == "Done")
+                {
+                    Console.WriteLine($"{storage - boxesCount} Cubic meters left.");
+                    endOfBoxes = true;
+                    break;
+                }
+                boxesCount += int.Parse(boxes);
+            }
+            if(endOfBoxes == false)
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(boxesCount - storage)} Cubic meters more.");
+            }
+        }
+    }
+}

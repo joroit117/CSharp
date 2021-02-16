6c62f3ad91518238f527f9f7deb3b450f7af07d5
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-02-16 17:45:26 +0200

    Add files via upload

---
+++ NestedLoops/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int comboCounter = 0;
+            bool foundCombo = false;
+
+            for(int i = startNumber; i <= endNumber; i++)
+                for (int j = startNumber; j <= endNumber; j++)
+                {
+                    comboCounter++;
+                    if(i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{comboCounter} ({i} + {j} = {magicNumber})");
+                        foundCombo = true;
+                        i = endNumber;
+                        j = endNumber;
+                    }
+                }
+            if(foundCombo == false)
+            {
+                Console.WriteLine($"{comboCounter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ NestedLoops/Travelling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while(destination != "End")
+            {
+                double neededSum = double.Parse(Console.ReadLine());
+                double budget = 0;
+
+                while(budget < neededSum)
+                {
+                    double savedMoney = double.Parse(Console.ReadLine());
+                    budget += savedMoney;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}

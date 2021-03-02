5f3b7d6a1d9e41a1a6c783426f7f5bb07d3737ec
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-02 14:49:58 +0200

    Add files via upload

---
+++ OnlineExam/SumAndProduct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumAndProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int a = number % 10000;
+            int b = number % 1000;
+            int c = number % 100;
+            int d = number % 10;
+
+            Console.WriteLine(a);
+
+        }
+    }
+}
---
+++ OnlineExam/SumAndProduct1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumAndProduct1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int a = rnd.Next(1, 9);
+            int b = rnd.Next(a, 9);
+            int c = rnd.Next(0, 9);
+            int d = rnd.Next(c, 9);
+            int k = (a * b * c * d) / (a + b + c + d);
+
+            if (a + b + c + d == a * b * c * d && number % 10 == 5)
+            {
+                int n = a * 1000 + b * 100 + c * 10 + d;
+                Console.WriteLine(n);
+            }
+            
+           else if(k == 3 && number % 3 == 0)
+            {
+                int n = d * 1000 + c * 100 + b * 10 + a;
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine("Nothing found");
+            }
+        }
+    }
+}

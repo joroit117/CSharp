d8289ac64581c1b22f31c72d6ed506d01a1fc921
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-11 09:21:53 +0200

    Add files via upload

---
+++ NestedLoops-Exercise/EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            for(int i = number1; i <= number2; i++)
+            {
+                string current = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+                for(int j = 0; j < current.Length; j++)
+                {
+                    if(j % 2 == 0)
+                    {
+                        evenSum += int.Parse(current[j].ToString());
+                    }
+                    else
+                    {
+                        oddSum += int.Parse(current[j].ToString());
+                    }
+                }
+                if(evenSum == oddSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ NestedLoops-Exercise/PasswordGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            string combination = "";
+
+            for(int i = 1; i < n; i++)
+                for(int j = 1; j < n; j++)
+                {
+                    int a = i;
+                    int b = j;
+                    for(int m = 0; m < l; m++)
+                        for(int k = 0; k < l; k++)
+                        {
+                            char c = Convert.ToChar(97 + m);
+                            char d = Convert.ToChar(97 + k);
+                            for (int s = a + 1; s <= n; s++)
+                            {
+                                if(s > a && s > b)
+                                {
+                                    int e = s;
+                                    combination += a.ToString();
+                                    combination += b.ToString();
+                                    combination += c.ToString();
+                                    combination += d.ToString();
+                                    combination += e.ToString();
+                                    Console.Write(combination + " ");
+                                    combination = "";
+                                }
+                            }
+                        }
+                }
+            }
+    }
+}
